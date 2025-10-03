bf2e3ab649e46b8d4e1dca5fb851defcc43858b1
Maciej Piotrowski <maciejpiotrowsk@gmail.com>
2025-10-03 13:46:13 +0200

    Fix compiler warnings.

--- Core/BankTransaction.cs
+++ Core/BankTransaction.cs
@@ -5,12 +5,12 @@ public class BankTransaction {
     public DateTime CurrencyDate { get; set; }
     public BankTransactionType TransactionType { get; set; }
     public decimal Amount { get; set; }
-    public string Currency { get; set; }
+    public string Currency { get; set; } = string.Empty;
     public decimal BalanceAfterTransaction { get; set; }
-    public string TransactionDescription { get; set; }
-    public string UnnamedProperty1 { get; set; }
-    public string UnnamedProperty2 { get; set; }
-    public string UnnamedProperty3 { get; set; }
+    public string TransactionDescription { get; set; } = string.Empty;
+    public string UnnamedProperty1 { get; set; } = string.Empty;
+    public string UnnamedProperty2 { get; set; } = string.Empty;
+    public string UnnamedProperty3 { get; set; } = string.Empty;
 
     // @@NOTE: Different information depeneding on the context
     // will be available at different times. If information is not available
