a4c8299509319d72a56391ee77818a839a2dc45d
Maciej Piotrowski <maciejpiotrowsk@gmail.com>
2025-10-03 13:47:52 +0200

    dotnet format

--- Core/BankTransactionType.cs
+++ Core/BankTransactionType.cs
@@ -1,15 +1,15 @@
 namespace FastPayment.Core;
 
 public enum BankTransactionType {
-    AtmWithdrawal,                // Wypłata z bankomatu
-    StandingOrder,                // Zlecenie stałe
-    CardPayment,                  // Płatność kartą
-    AccountTransfer,              // Przelew na konto
-    IncomingPhoneTransfer,        // Przelew na telefon przychodz. zew.
-    MobileWebPayment,             // Płatność web - kod mobilny
-    Commission,                   // Prowizja
-    VariableOrder,                // Zlecenie zmienne
-    OutgoingAccountTransfer,      // Przelew z rachunku
-    ForeignCurrencyTransfer,      // Przelew zagraniczny i walutowy
-    AtmWithdrawalMobileCode       // Wypłata w bankomacie - kod mobilny
+  AtmWithdrawal,                // Wypłata z bankomatu
+  StandingOrder,                // Zlecenie stałe
+  CardPayment,                  // Płatność kartą
+  AccountTransfer,              // Przelew na konto
+  IncomingPhoneTransfer,        // Przelew na telefon przychodz. zew.
+  MobileWebPayment,             // Płatność web - kod mobilny
+  Commission,                   // Prowizja
+  VariableOrder,                // Zlecenie zmienne
+  OutgoingAccountTransfer,      // Przelew z rachunku
+  ForeignCurrencyTransfer,      // Przelew zagraniczny i walutowy
+  AtmWithdrawalMobileCode       // Wypłata w bankomacie - kod mobilny
 }
