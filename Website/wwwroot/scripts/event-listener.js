89b4b7b063dea7c32c2b70492ac8de3d029e885f
Maciej Piotrowski <maciejpiotrowsk@gmail.com>
2025-10-10 00:12:57 +0200

    Working with different version of handling key presses

--- Website/wwwroot/scripts/event-listener.js
+++ Website/wwwroot/scripts/event-listener.js
@@ -21,3 +21,29 @@ window.JsFunctions = {
         });
     }
 };
+
+window.EnableKeyboardCapture = (componentRef) => {
+  document.onkeydown = function (ev) {
+    const serialized = serializeEvent(ev);
+    console.log(ev.key);
+    raiseKeyDownEvent(ev, serialized);
+  }
+
+  function raiseKeyDownEvent(ev, serialized) {
+    componentRef.invokeMethodAsync('JsKeyDown', serialized);
+  }
+
+  function serializeEvent(ev) {
+    return {
+      key:      ev.key,
+      code:     ev.keyCode.toString(),
+      location: ev.location,
+      repeat:   ev.repeat,
+      ctrlKey:  ev.ctrlKey,
+      shiftKey: ev.shiftKey,
+      altKey:   ev.altKey,
+      metaKey:  ev.metaKey,
+      type:     ev.type
+    }
+  }
+}
